a38a365fd13c987d6900b065cc1ee5c661a5dec9
Robert Kolner <robert.kolner@gmail.com>
2016-12-10 16:23:15 +0100

    Added flake8 and local settings

--- hdo_messenger_backend/settings.py
+++ hdo_messenger_backend/settings.py
@@ -123,3 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# API keys
+FACEBOOK_APP_ACCESS_TOKEN = os.getenv('FACEBOOK_APP_ACCESS_TOKEN')
+
+
+try:
+    from .local_settings import *  # noqa
+except ImportError:
+    pass  # All good!
