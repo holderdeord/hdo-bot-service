7f5ffaddaba77d12d53b2965d70391b843e70ab3
Nikolai R Kristiansen <nikolaik@gmail.com>
2017-01-01 17:26:56 +0100

    s/messenger_backend/quiz_service/

--- hdo_quiz_service/urls.py
+++ hdo_quiz_service/urls.py
@@ -1,4 +1,4 @@
-"""hdo_messenger_backend URL Configuration
+"""hdo_quiz_service URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
