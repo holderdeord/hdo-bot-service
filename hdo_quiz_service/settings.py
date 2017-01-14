eb23a2b2b6dba0f6dc8a2502fff60bbf984b7641
Robert Kolner <robert.kolner@gmail.com>
2017-01-14 14:45:11 +0100

    Added staticfiles and upgraded runtime

--- hdo_quiz_service/settings.py
+++ hdo_quiz_service/settings.py
@@ -146,7 +146,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # API keys
 FACEBOOK_APP_ACCESS_TOKEN = os.getenv('FACEBOOK_APP_ACCESS_TOKEN')
