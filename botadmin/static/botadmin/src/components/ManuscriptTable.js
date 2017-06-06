c623c20de342e064f40a754b096476be3f678693
Arne Hassel <arne.hassel@gmail.com>
2017-06-06 21:44:27 +0200

    Added columns number of items and updated

--- botadmin/static/botadmin/src/components/ManuscriptTable.js
+++ botadmin/static/botadmin/src/components/ManuscriptTable.js
@@ -10,16 +10,20 @@ const ManuscriptTable = ({ manuscripts, deleteManuscript }) => (
       <tr>
         <th>#</th>
         <th>Name</th>
+        <th># Items</th>
+        <th>Last Updated</th>
         <th>Actions</th>
       </tr>
       </thead>
       <tbody>
-      { manuscripts.map(({ pk, name, category }, index) => (
+      { manuscripts.map(({ pk, name, items, updated }, index) => (
         <tr key={ `manuscript-${index}` }>
           <td>{ pk }</td>
           <td>
             <Link to={`/edit/${ pk }`}>{ name }</Link>
           </td>
+          <td>{items.length}</td>
+          <td>{updated}</td>
           <td>
             <button className="btn btn-default" type="button" onClick={() => deleteManuscript(pk)}>
               <span className="glyphicon glyphicon-trash"></span>
