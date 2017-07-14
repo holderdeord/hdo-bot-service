d5f1b8e5ff1b91bbcd397d7804ff10cbbb8734f1
Arne Hassel <arne.hassel@gmail.com>
2017-07-14 15:33:23 +0200

    Expanding importing electoral guide script to link manuscripts

--- quiz/management/commands/sync_electoral_guide.py
+++ quiz/management/commands/sync_electoral_guide.py
@@ -21,15 +21,25 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         # logging.info('test', options['category_map'], options['file'])
-        manuscripts = self.get_manuscripts(options['file'])
-        for manuscript_name in manuscripts:
-            hdo_category = self.get_hdo_category(manuscripts[manuscript_name]['hdo_category'])
+        manuscripts_data = self.get_manuscripts_data(options['file'])
+        manuscripts = self.add_manuscripts_to_db(manuscripts_data)
+        root_manuscript = Manuscript.objects.get_or_create(name='Valgomat start')
+        hdo_categories = self.group_into_hdo_categories(manuscripts)
+        for hdo_category in hdo_categories:
+            self.link_manuscripts(hdo_category, root_manuscript)
+            # logging.info(hdo_categories)
+
+    def add_manuscripts_to_db(self, manuscripts_data):
+        manuscripts = {}
+        for manuscript_data in manuscripts_data:
+            hdo_category = self.get_hdo_category(manuscript_data['hdo_category'])
             manuscript, created = Manuscript.objects.get_or_create(
-                name=manuscript_name,
+                name=manuscript_data['name'],
                 type='voter_guide',
                 hdo_category=hdo_category
             )
-            for alternative_data in manuscripts[manuscript_name]['alternatives']:
+            manuscripts[manuscript.pk] = manuscript
+            for alternative_data in manuscript_data['alternatives']:
                 alternative, created = VoterGuideAlternative.objects.get_or_create(
                     text=alternative_data['text'],
                     manuscript=manuscript
@@ -38,8 +48,9 @@ class Command(BaseCommand):
                 promises = list(map(self.get_promise, alternative_data['promises']))
                 alternative.promises.add(*promises)
                 alternative.save()
+        return [v for v in manuscripts.values()]
 
-    def get_manuscripts(self, file_path):
+    def get_manuscripts_data(self, file_path):
         manuscripts = {}
         with open(file_path) as f:
             csv_file = csv.DictReader(f)
@@ -51,11 +62,50 @@ class Command(BaseCommand):
                     manuscripts[manuscript_name]['alternatives'].append(self.create_alternative(row))
                 else:
                     manuscripts[manuscript_name] = {
+                        'name': manuscript_name,
                         'hdo_category': row['HDO-kategori'],
                         'manuscript_name': row['Tema'],
                         'alternatives': [self.create_alternative(row)]
                     }
-        return manuscripts
+        return [v for v in manuscripts.values()]
+
+    def find_linked_manuscripts(self, hdo_category):
+        linked_manuscripts = []
+        unlinked_manuscripts = []
+        for manuscript in hdo_category['manuscripts']:
+            if manuscript.items.count() > 0:
+                linked_manuscripts.append(manuscript)
+            else:
+                unlinked_manuscripts.append(manuscript)
+        return linked_manuscripts, unlinked_manuscripts
+
+    def group_into_hdo_categories(self, manuscripts):
+        hdo_categories = {}
+        for manuscript in manuscripts:
+            hdo_category_pk = manuscript.hdo_category.pk
+            if hdo_category_pk in hdo_categories:
+                hdo_categories[hdo_category_pk]['manuscripts'].append(manuscript)
+            else:
+                hdo_categories[hdo_category_pk] = {
+                    'category': manuscript.hdo_category,
+                    'manuscripts': [manuscript]
+                }
+        return [v for v in hdo_categories.values()]
+
+    def link_manuscripts(self, hdo_category, root_manuscript):
+        linked_manuscripts, unlinked_manuscripts = self.find_linked_manuscripts(hdo_category)
+        number_of_unlinked_manuscripts = len(unlinked_manuscripts)
+        for index, manuscript in enumerate(unlinked_manuscripts):
+            if index + 1 < number_of_unlinked_manuscripts:
+                self.createManuscriptItemThatLinks(manuscript, unlinked_manuscripts[index + 1])
+            elif len(linked_manuscripts) > 0:
+                logging.info('continue adding')
+            else:
+                logging.info('end stuff')
+
+    def createManuscriptItemThatLinks(self, current_manuscript, next_manuscript):
+
+        logging.info('link stuff')
 
     def create_alternative(self, row):
         def strip_promise(promise):
