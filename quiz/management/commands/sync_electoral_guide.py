5c0cafc7d641371025225266b8b986029edca1c4
Arne Hassel <arne.hassel@gmail.com>
2017-07-16 16:13:31 +0200

    Should now import manuscripts properly from valgomat spreadsheet

--- quiz/management/commands/sync_electoral_guide.py
+++ quiz/management/commands/sync_electoral_guide.py
@@ -1,8 +1,9 @@
 import csv
 import logging
 from django.core.management import BaseCommand
+from django.db import connection
 
-from quiz.models import Manuscript, HdoCategory, VoterGuideAlternative, Promise
+from quiz.models import Manuscript, HdoCategory, VoterGuideAlternative, Promise, ManuscriptItem
 
 
 class Command(BaseCommand):
@@ -20,14 +21,12 @@ class Command(BaseCommand):
                             help='Import manuscripts from file')
 
     def handle(self, *args, **options):
-        # logging.info('test', options['category_map'], options['file'])
         manuscripts_data = self.get_manuscripts_data(options['file'])
         manuscripts = self.add_manuscripts_to_db(manuscripts_data)
-        root_manuscript = Manuscript.objects.get_or_create(name='Valgomat start')
+        root_manuscript = self.create_root_manuscript()
         hdo_categories = self.group_into_hdo_categories(manuscripts)
         for hdo_category in hdo_categories:
             self.link_manuscripts(hdo_category, root_manuscript)
-            # logging.info(hdo_categories)
 
     def add_manuscripts_to_db(self, manuscripts_data):
         manuscripts = {}
@@ -39,16 +38,31 @@ class Command(BaseCommand):
                 hdo_category=hdo_category
             )
             manuscripts[manuscript.pk] = manuscript
-            for alternative_data in manuscript_data['alternatives']:
-                alternative, created = VoterGuideAlternative.objects.get_or_create(
-                    text=alternative_data['text'],
-                    manuscript=manuscript
-                )
-                alternative.save()
-                promises = list(map(self.get_promise, alternative_data['promises']))
-                alternative.promises.add(*promises)
-                alternative.save()
+            if created:
+                for alternative_data in manuscript_data['alternatives']:
+                    alternative, created = VoterGuideAlternative.objects.get_or_create(
+                        text=alternative_data['text'],
+                        manuscript=manuscript
+                    )
+                    alternative.save()
+                    promises = list(map(self.get_promise, alternative_data['promises']))
+                    alternative.promises.add(*promises)
+                    alternative.save()
+                self.create_starting_manuscript_item(manuscript)
         return [v for v in manuscripts.values()]
+
+    def create_root_manuscript(self):
+        manuscript, created = Manuscript.objects.get_or_create(
+            name='Valgomat start',
+            is_default=True
+        )
+        if created:
+            ManuscriptItem.objects.get_or_create(
+                type='vg_categories',
+                manuscript=manuscript,
+                text='Velg den kategorien du syns er mest interessant.'
+            )
+        return manuscript
 
     def get_manuscripts_data(self, file_path):
         manuscripts = {}
@@ -73,7 +87,7 @@ class Command(BaseCommand):
         linked_manuscripts = []
         unlinked_manuscripts = []
         for manuscript in hdo_category['manuscripts']:
-            if manuscript.items.count() > 0:
+            if manuscript.items.count() > 1:
                 linked_manuscripts.append(manuscript)
             else:
                 unlinked_manuscripts.append(manuscript)
@@ -94,18 +108,78 @@ class Command(BaseCommand):
 
     def link_manuscripts(self, hdo_category, root_manuscript):
         linked_manuscripts, unlinked_manuscripts = self.find_linked_manuscripts(hdo_category)
+        number_of_linked_manuscripts = len(linked_manuscripts)
         number_of_unlinked_manuscripts = len(unlinked_manuscripts)
+        # link unlinked manuscripts
         for index, manuscript in enumerate(unlinked_manuscripts):
             if index + 1 < number_of_unlinked_manuscripts:
-                self.createManuscriptItemThatLinks(manuscript, unlinked_manuscripts[index + 1])
-            elif len(linked_manuscripts) > 0:
-                logging.info('continue adding')
+                next_manuscript = unlinked_manuscripts[index + 1]
+                manuscript.next = next_manuscript
+                self.create_manuscript_item_that_links_to_next_manuscript(manuscript, next_manuscript)
             else:
-                logging.info('end stuff')
+                manuscript.next = root_manuscript
+                self.create_manuscript_item_that_links_to_root_manuscript(manuscript, root_manuscript)
+            manuscript.save()
+        # link newly linked manuscripts to previously linked manuscripts
+        if number_of_linked_manuscripts > 0 and number_of_unlinked_manuscripts > 0:
+            last_manuscript = next(x for x in linked_manuscripts if x.next == root_manuscript)
+            first_manuscript = unlinked_manuscripts[0]
+            # remove current items
+            last_manuscript.items.all().delete()
+            # add new items
+            self.create_starting_manuscript_item(last_manuscript)
+            self.create_manuscript_item_that_links_to_next_manuscript(last_manuscript, first_manuscript)
+            # update next
+            last_manuscript.next = first_manuscript
+            last_manuscript.save()
+        # set is_first_in_category on first manuscript, if necessary
+        if number_of_linked_manuscripts == 0:
+            unlinked_manuscripts[0].is_first_in_category = True
+            unlinked_manuscripts[0].save()
 
-    def createManuscriptItemThatLinks(self, current_manuscript, next_manuscript):
+    def create_starting_manuscript_item(self, manuscript):
+        ManuscriptItem.objects.get_or_create(
+            type='vg_questions',
+            manuscript=manuscript,
+            text='Hvilken tekst er du mest enig i?'
+        )
 
-        logging.info('link stuff')
+    def create_manuscript_item_that_links_to_root_manuscript(self, current_manuscript, root_manuscript):
+        ManuscriptItem.objects.get_or_create(
+            type='vg_result',
+            manuscript=current_manuscript,
+            text='Du har nå gått gjennom alle spørsmålene vi har for denne kategorien, og her partiene vi tror du er '
+                 'mest enig i.'
+        )
+        ManuscriptItem.objects.get_or_create(
+            type='quick_reply',
+            manuscript=current_manuscript,
+            text='Om du vil kan du velge en annen kategori og besvare spørsmålene for å gjøre vår gjetting bedre.',
+            reply_text_1='Vis meg de andre kategoriene',
+            reply_action_1=root_manuscript
+        )
+
+    def create_manuscript_item_that_links_to_next_manuscript(self, current_manuscript, next_manuscript):
+        ManuscriptItem.objects.get_or_create(
+            type='quick_reply',
+            manuscript=current_manuscript,
+            text='Vil du se foreløpig resultat, eller vil du gå videre til neste spørsmål?',
+            reply_text_1='Foreløpig resultat',
+            reply_text_2='Neste spørsmål',
+            reply_action_2=next_manuscript
+        )
+        ManuscriptItem.objects.get_or_create(
+            type='vg_result',
+            manuscript=current_manuscript,
+            text='Her er foreløpige resultat for partiene vi tror du er mest enig i'
+        )
+        ManuscriptItem.objects.get_or_create(
+            type='quick_reply',
+            manuscript=current_manuscript,
+            text='Når du er klar kan du gå videre til å se neste spørsmål.',
+            reply_text_1='Vis meg neste spørsmål',
+            reply_action_1=next_manuscript
+        )
 
     def create_alternative(self, row):
         def strip_promise(promise):
