3f6c8d22ba82590761b50f92c5b587859351a795
Arne Hassel <arne.hassel@gmail.com>
2017-08-05 03:05:00 +0200

    Added Vet ikke-alternative

--- quiz/management/commands/sync_electoral_guide.py
+++ quiz/management/commands/sync_electoral_guide.py
@@ -48,7 +48,21 @@ class Command(BaseCommand):
                     promises = list(map(self.get_promise, alternative_data['promises']))
                     alternative.promises.add(*promises)
                     alternative.save()
+                VoterGuideAlternative.objects.get_or_create(
+                    text='Vet ikke eller syns ingen av alternativene er gode',
+                    manuscript=manuscript
+                )
                 self.create_starting_manuscript_item(manuscript)
+            else:
+                alternatives = VoterGuideAlternative.objects.filter(
+                    manuscript=manuscript,
+                    text='Vet ikke eller syns ingen av alternativene er gode'
+                )
+                if alternatives.count() == 0:
+                    VoterGuideAlternative.objects.get_or_create(
+                        text='Vet ikke eller syns ingen av alternativene er gode',
+                        manuscript=manuscript
+                    )
         return [v for v in manuscripts.values()]
 
     def create_root_manuscript(self):
